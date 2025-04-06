9114cf8ddfafe87e3830176ab2e2fff0dfaea8ec
seonghyun <codiee@naver.com>
2025-04-06 19:36:58 +0900

    Env: 개발환경 초기화

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        customRed: "#ef4444",
+      },
+    },
+  },
+  plugins: [],
+};
