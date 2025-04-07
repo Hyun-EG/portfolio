1b7b73d576d280eca4d2b962f706206ae0cce07b
Hyun-EG <codiee@naver.com>
2025-04-07 21:36:02 +0900

    Add: Nav 컴포넌트 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,19 @@
 import IntroBox from "@/components/intro/IntroBox";
 import ProfileBox from "@/components/profile/ProfileBox";
-import React from "react";
+import ProjectBox from "@/components/project/ProjectBox";
+import EduBox from "@/components/education/EduBox";
+import OutroBox from "@/components/outro/OutroBox";
+import Nav from "@/components/nav/NavBox";
 
-const page = () => {
+export default function Home() {
   return (
-    <div>
+    <>
+      <Nav />
       <IntroBox />
       <ProfileBox />
-    </div>
+      <ProjectBox />
+      <EduBox />
+      <OutroBox />
+    </>
   );
-};
-
-export default page;
+}
