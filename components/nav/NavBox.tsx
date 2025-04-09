980dbfa713086a7fbecfb9f113bcb65e2a297f9a
Hyun-EG <codiee@naver.com>
2025-04-09 16:37:36 +0900

    Style: 투명화 애니메이션 추가

--- components/nav/NavBox.tsx
+++ components/nav/NavBox.tsx
@@ -6,14 +6,19 @@ import { RootState } from "@/store/store";
 const NavBox = () => {
   const isVisible = useSelector((state: RootState) => state.nav.isVisible);
 
-  if (!isVisible) return null;
-
   return (
-    <div className="fixed left-0 top-0 w-96 h-screen flex flex-col text-white bg-bgBlack z-50">
+    <div
+      className={`fixed left-0 top-0 w-96 h-screen p-5 rounded-r-3xl flex flex-col gap-4 text-white bg-bgBlack z-50 transition-opacity duration-[500ms] ${
+        isVisible
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
+      }`}
+    >
       <span className="text-3xl cursor-pointer">Intro</span>
       <span className="text-3xl cursor-pointer">Profile</span>
       <span className="text-3xl cursor-pointer">Projects</span>
       <span className="text-3xl cursor-pointer">Educations</span>
+      <span className="text-3xl cursor-pointer">Outro</span>
     </div>
   );
 };
