8cd3a4cba99b83cf1d93f282198d3a0e14fe7bdd
seonghyun <codiee@naver.com>
2025-04-10 20:53:45 +0900

    Add: CardProps 타입 추가

---
+++ types/cardProps.ts
@@ -0,0 +1,8 @@
+import { StaticImageData } from "next/image";
+
+export type CardProps = {
+  projectImg: StaticImageData;
+  projectName: string;
+  projectDesc: string;
+  badges: React.ReactNode;
+};
