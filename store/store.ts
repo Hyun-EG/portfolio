531e4fa95f4d571781f53e176e2264399db3d13d
seonghyun <codiee@naver.com>
2025-04-13 23:45:48 +0900

    Add: 디테일모달, 선택한프로젝트 slice 연결

--- store/store.ts
+++ store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import navReducer from "../features/nav/navSlice";
 import projectReducer from "../features/project/projectSlice";
+import detailImageModalReducer from "../features/detailImageModal/detailImageModalSlice";
+import selectedProjectReducer from "@/features/selectedProject/selectedProjectSlice";
 
 export const store = configureStore({
   reducer: {
     nav: navReducer,
     project: projectReducer,
+    detailImgModal: detailImageModalReducer,
+    selectedProject: selectedProjectReducer,
   },
 });
 
