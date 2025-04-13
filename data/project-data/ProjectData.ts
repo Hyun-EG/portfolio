91e4bb680160a26f56a528620b6df6d019176381
seonghyun <codiee@naver.com>
2025-04-13 23:42:29 +0900

    Add: 프로젝트 데이터용 파일 추가 셀웹 추가중

---
+++ data/project-data/ProjectData.ts
@@ -0,0 +1,132 @@
+// ProjectData.ts
+import { ProjectDataType } from "@/types/projectDataType";
+
+export const projectData: ProjectDataType = {
+  sellweb: {
+    title: "Sell Web (셀웹)",
+    description:
+      "Next.js API Routes를 활용한 서버리스 아키텍처 기반의 풀스택 프로젝트입니다. 결제 모듈과 외부 라이브러리 연동, OAuth 기반 인증 및 쿠키 관리, Tailwind CSS를 활용한 UI 구현, 기획부터 Figma 디자인까지 전 과정을 2人에서 경험하며 실전 중심의 역량 강화를 목표로 진행했습니다.",
+    date: "2025.01.07 ~ 진행중",
+    team: "FE 2명",
+    images: {
+      img1: "/images/projects/sellweb/home.PNG",
+      img2: "/images/projects/sellweb/login.PNG",
+      img3: "/images/projects/sellweb/signup.PNG",
+    },
+    keyFeatures: [
+      "소셜 로그인 및 자체 인증 시스템 병행 (Naver SMTP 연동)",
+      "템플릿 목록 및 결제 기능",
+      "사용자 문의 / 공지 사항 / 구매 내역 / 찜 내역 / 마이 페이지",
+      "관리자 전용 페이지: 공지/문의관리, 템플릿 CRUD, 광고 관리, 인기 템플릿 차트 시각화",
+      "Mongoose 기반 스키마 설계 주요 정보 해시 처리",
+      "REST API 설계 및 백엔드 로직 구현",
+    ],
+    skills: [
+      {
+        skill: "React",
+        content: "정말 열심히 했어요",
+      },
+    ],
+    contribution: "",
+  },
+  karb: {
+    title: "KARB (한국광고 심의기관)",
+    description:
+      "외주 개발 사기이슈가 많아서 미리 만들어놓고 파는 템플릿 사이트를 만들어봤습니다",
+    date: "2024.08.05 ~ 2024.09.20",
+    team: "PM 3명 UI/UX 3명 FE 3명 BE 3명",
+    images: {
+      img1: "/images/projects/sellweb/home.PNG",
+      img2: "/images/projects/sellweb/login.PNG",
+      img3: "/images/projects/sellweb/signup.PNG",
+    },
+    keyFeatures: [""],
+    skills: [
+      {
+        skill: "React",
+        content: "정말 열심히 했어요",
+      },
+    ],
+    contribution: "",
+  },
+  aircnc: {
+    title: "Aircnc",
+    description:
+      "외주 개발 사기이슈가 많아서 미리 만들어놓고 파는 템플릿 사이트를 만들어봤습니다",
+    date: "2024.06.17 ~ 2024.07.07",
+    team: "FE 3명 BE 4명",
+    images: {
+      img1: "/images/projects/sellweb/home.PNG",
+      img2: "/images/projects/sellweb/login.PNG",
+      img3: "/images/projects/sellweb/signup.PNG",
+    },
+    keyFeatures: [""],
+    skills: [
+      {
+        skill: "React",
+        content: "정말 열심히 했어요",
+      },
+    ],
+    contribution: "",
+  },
+  devtube: {
+    title: "Devtube",
+    description:
+      "외주 개발 사기이슈가 많아서 미리 만들어놓고 파는 템플릿 사이트를 만들어봤습니다",
+    date: "2024.05.27 ~ 2024.06.09",
+    team: "FE 4명",
+    images: {
+      img1: "/images/projects/sellweb/home.PNG",
+      img2: "/images/projects/sellweb/login.PNG",
+      img3: "/images/projects/sellweb/signup.PNG",
+    },
+    keyFeatures: [""],
+    skills: [
+      {
+        skill: "React",
+        content: "정말 열심히 했어요",
+      },
+    ],
+    contribution: "",
+  },
+  intranet: {
+    title: "Intranet",
+    description:
+      "외주 개발 사기이슈가 많아서 미리 만들어놓고 파는 템플릿 사이트를 만들어봤습니다",
+    date: "2025/01/07 ~ 진행중",
+    team: "FE 4명",
+    images: {
+      img1: "/images/projects/sellweb/home.PNG",
+      img2: "/images/projects/sellweb/login.PNG",
+      img3: "/images/projects/sellweb/signup.PNG",
+    },
+    keyFeatures: [""],
+    skills: [
+      {
+        skill: "React",
+        content: "정말 열심히 했어요",
+      },
+    ],
+    contribution: "",
+  },
+  ppoppis: {
+    title: "PPOPPIS",
+    description:
+      "외주 개발 사기이슈가 많아서 미리 만들어놓고 파는 템플릿 사이트를 만들어봤습니다",
+    date: "2025/03/30 ~ 진행중",
+    team: "개인",
+    images: {
+      img1: "/images/projects/sellweb/home.PNG",
+      img2: "/images/projects/sellweb/login.PNG",
+      img3: "/images/projects/sellweb/signup.PNG",
+    },
+    keyFeatures: [""],
+    skills: [
+      {
+        skill: "React",
+        content: "정말 열심히 했어요",
+      },
+    ],
+    contribution: "",
+  },
+};
