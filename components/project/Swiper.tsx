58c2061ff3f48319901fc424fdfe1c5689af7ccd
seonghyun <codiee@naver.com>
2025-04-16 20:56:47 +0900

    Style: 스타일 수정

--- components/project/Swiper.tsx
+++ components/project/Swiper.tsx
@@ -63,7 +63,7 @@ const Swiper = ({ images }: SwiperProps) => {
           key={index}
           width={260}
           height={176}
-          className="w-1/4 h-44 rounded-xl cursor-pointer object-cover"
+          className="w-1/4 h-44 rounded-xl cursor-pointer"
         />
       ))}
       <span
